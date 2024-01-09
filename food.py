2fc02aa87a1da110adf316f1c35ad86d860f203b
Sakib_Dalal <sakibdalal73@gmail.com>
2024-01-09 15:36:47 +0530

    add random color to food

--- food.py
+++ food.py
@@ -1,18 +1,27 @@
-from turtle import Turtle
+import turtle
 import random
 
+turtle.colormode(255)
 
-class Food(Turtle):
+
+class Food(turtle.Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
+        self.change_food_color()
         self.speed("fastest")
         self.refresh()
 
+    def change_food_color(self):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        self.color(r, g, b)
+
     def refresh(self):
+        self.change_food_color()
         random_x = random.randint(-270, 270)
         random_y = random.randint(-270, 270)
         self.goto(x=random_x, y=random_y)
