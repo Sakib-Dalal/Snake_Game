96a8cc976fd5b7d4a16045d9f1ff8a19c316422a
Sakib_Dalal <sakibdalal73@gmail.com>
2024-01-09 15:41:47 +0530

    change food shape to turtle

--- food.py
+++ food.py
@@ -7,9 +7,9 @@ turtle.colormode(255)
 class Food(turtle.Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.shapesize(stretch_len=0.8, stretch_wid=0.8)
         self.change_food_color()
         self.speed("fastest")
         self.refresh()
